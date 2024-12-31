480340146920b1bccff051649810e1b660dc16d7
mickamy <tetsuro@mickamy.com>
2024-12-31 14:01:14 +0900

    config: use english

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,13 @@
+require "sidekiq/web"
+
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV["REDIS_URL"] }
 end
 
 Sidekiq.configure_client do |config|
   config.redis = { url: ENV["REDIS_URL"] }
+end
+
+Sidekiq::Web.use Rack::Config do |env|
+  env["rack.locale"] = "en"
 end
